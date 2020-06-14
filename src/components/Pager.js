bae44df18b4fafc17e3ade72f1fb860557f68134
Andre K W <andre.k.willie@gmail.com>
2020-06-14 05:56:45 -0400

    added page number changer

--- src/components/Pager.js
+++ src/components/Pager.js
@@ -3,13 +3,31 @@ import React from 'react';
 export default function Pager(props) {
   const prevBtn = <button type="button" onClick={e => props.setPageNumber(props.pageNumber - 1)}>{props.pageNumber - 1}</button>;
 
+  const inputPageNumber = (e) => {
+    e.preventDefault();
+
+    const num = parseInt(e.target.pagenum.value);
+
+    if(isNaN(num)) return;
+
+    props.setPageNumber(num);
+  };
+
   return (
     <div className={`pager ${props.hide ? 'hide' : ''}`}>
-      <p>
+      <div><small>hey</small></div>
+
+      <div>
         {props.pageNumber > 1 && [prevBtn, <span>... </span>]}
         <span>{props.pageNumber} ...</span>
         <button type="button" onClick={e => props.setPageNumber(props.pageNumber + 1)}>{props.pageNumber + 1}</button>
-      </p>
+      </div>
+
+      <div>
+        <form id="pager-search" onSubmit={inputPageNumber}>
+          <input name="pagenum" type="text" />
+        </form>
+      </div>
     </div>
   );
 }
