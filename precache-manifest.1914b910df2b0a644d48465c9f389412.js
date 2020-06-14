f63a53cc2a10f9fc04ae910e2476a299731c7f77
Andre K W <andre.k.willie@gmail.com>
2020-06-14 18:41:32 -0400

    Updates

---
+++ precache-manifest.1914b910df2b0a644d48465c9f389412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82df9f5912b9a168ea275b552171337b",
+    "url": "/yeetmeister/index.html"
+  },
+  {
+    "revision": "5ad398a983b2744eeeb1",
+    "url": "/yeetmeister/static/css/main.5fdb9484.chunk.css"
+  },
+  {
+    "revision": "c9736900563a18565698",
+    "url": "/yeetmeister/static/js/2.4b7d7bc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yeetmeister/static/js/2.4b7d7bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad398a983b2744eeeb1",
+    "url": "/yeetmeister/static/js/main.41087dc7.chunk.js"
+  },
+  {
+    "revision": "694ea7e5b6362c55be83",
+    "url": "/yeetmeister/static/js/runtime-main.4b64e005.js"
+  }
+]);
